6844f16ea0dc957ba99795d91bc52e807915b7ef
Vilmos Yehuda Frank <vilmosyehudafrank@Vilmoss-MBP.lan>
2026-04-29 21:29:55 +0300

    chore: add gitignore and remove generated files

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/LiveDashboard.tsx -> @/utils/countries\":{\"id\":\"components/LiveDashboard.tsx -> @/utils/countries\",\"files\":[]}}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k1XMlIWUz/WQl4wogITpdvc33/uvLSnzPYXdRTfppjU=\"\n}"
