e6f4ddb4e64530bcedcc7a39894adfa0b8a5339d
Vilmos Yehuda Frank <vilmosyehudafrank@Vilmoss-MacBook-Pro.local>
2026-05-05 09:47:45 +0300

    fix: Add backwards-compatible redirects for old locale routes

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,20 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:locale(en|de|fr|es|pt|ru|ko|hu)',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:locale(en|de|fr|es|pt|ru|ko|hu)/:path*',
+        destination: '/:path*',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
